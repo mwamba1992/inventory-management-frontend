ef94f1f43a69c16e0aaf4dea6557e2abcd20735c
JOEL MSUNGU GAITAN <joel.gaitan@hazina.go.tz>
2025-07-29 11:06:03 +0300

    base ip change

--- src/utils/Configs.js
+++ src/utils/Configs.js
@@ -1,6 +1,6 @@
 export const Configs = {
   //API_BASE_URL: 'http://localhost:3000',
-  API_BASE_URL: 'http://business.mwendavano.com:3000',
+  API_BASE_URL: 'https://business.mwendavano.com/api',
   REPORT_BASE_URL: 'https://trais.mof.go.tz:85'
 };
 
